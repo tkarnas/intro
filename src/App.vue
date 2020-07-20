84446fcbfe11063dbafe0c49891243d4f73729c9
tkarnas <46887297+tkarnas@users.noreply.github.com>
2020-07-20 13:05:02 +0200

    Add files via upload
    Just intro

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <Intro class="intro" />
+  </div>
+</template>
+
+<script>
+import Intro from "./components/Intro.vue";
+
+export default {
+  name: "App",
+  components: {
+    Intro
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+  color: #2c3e50;
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.intro {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  background: #2c3e50;
+  color: antiquewhite;
+}
+</style>
