4f95cd38774345aaad8b2f49fa9ef6270ce0febe
tkarnas <46887297+tkarnas@users.noreply.github.com>
2020-08-18 13:58:46 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,128 @@
+<template>
+  <div id="app">
+    <Intro class="intro" />
+  </div>
+</template>
+
+<script>
+import Intro from "./components/Intro.vue";
+
+export default {
+  name: "App",
+  components: {
+    Intro,
+  },
+  data: () => {
+    return {
+      display: "none",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+@import "https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+  color: #2c3e50;
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.intro {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 99;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  background: rgb(26, 26, 26);
+  color: tomato;
+  animation: slide 1s linear forwards;
+  animation-delay: 4s;
+}
+.bm-burger-bars {
+  background-color: tomato;
+  animation: burger 5s linear forwards;
+}
+
+.bm-menu {
+  background-color: rgb(68, 68, 68);
+}
+
+.bm-item-list > * > span {
+  color: tomato;
+}
+header {
+  display: flex;
+  width: 80%;
+  margin: auto;
+  align-items: center;
+  height: 5vh;
+}
+.nav {
+  flex: 1;
+  background: greenyellow;
+}
+
+.nav__list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nav__item {
+  margin: 0 2rem;
+}
+
+.logo {
+  display: flex;
+  background: cornflowerblue;
+}
+
+.logo img {
+  display: block;
+  height: 3vh;
+}
+
+@keyframes burger {
+  0% {
+    scale: 0%;
+    opacity: 0;
+  }
+  90% {
+    scale: 0%;
+    opacity: 0;
+  }
+  100% {
+    scale: 100%;
+    opacity: 1;
+  }
+}
+
+@keyframes slide {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+    display: none;
+  }
+}
+</style>
---
+++ components/Intro.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="intro-container">
+    <div class="name">
+      <h2>{{ string }}</h2>
+      <h1>{{ string2 }}</h1>
+    </div>
+    <div class="introMessage2">
+      <p class="introMessage">{{ message }}</p>
+      <p class="introMessage">{{ message2 }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Intro",
+  props: {},
+  data: () => {
+    return {
+      array: ["T", "O", "M", "I", "S", "L", "A", "V "],
+      array2: ["K", "A", "R", "N", "A", "S"],
+      array3: ["f", "r", "o", "n", "t", "-", "e", "n", "d"],
+      array4: ["d", "e", "v", "e", "l", "o", "p", "e", "r"],
+      string: "",
+      string2: "",
+      message: "",
+      message2: "",
+    };
+  },
+  methods: {
+    appendFirstName: function () {
+      const it = this.array[Symbol.iterator](); // convenient for yeilding values
+      const int = setInterval(() => {
+        // time interval
+        const next = it.next(); // next value
+        if (!next.done) {
+          // done = true when the end of array reached
+          this.string += " " + next.value; // concatenate word to the string
+        } else {
+          clearInterval(int); // when done - clear interval
+        }
+      }, 150); // interval duration,
+    },
+    appendLastName: function () {
+      setTimeout(() => {
+        const it = this.array2[Symbol.iterator](); // convenient for yeilding values
+        const int = setInterval(() => {
+          // time interval
+          const next = it.next(); // next value
+          if (!next.done) {
+            // done = true when the end of array reached
+            this.string2 += " " + next.value; // concatenate word to the string
+          } else {
+            clearInterval(int); // when done - clear interval
+          }
+        }, 150); // interval duration, 1s
+      }, 1200);
+    },
+    appendMsg: function () {
+      setTimeout(() => {
+        const it = this.array3[Symbol.iterator](); // convenient for yeilding values
+        const int = setInterval(() => {
+          // time interval
+          const next = it.next(); // next value
+          if (!next.done) {
+            // done = true when the end of array reache
+            this.message += " " + next.value; // concatenate word to the string
+          } else {
+            clearInterval(int); // when done - clear interval
+          }
+        }, 100); // interval duration, 1s
+      }, 2100);
+    },
+    appendMsg2: function () {
+      setTimeout(() => {
+        const it = this.array4[Symbol.iterator](); // convenient for yeilding values
+        const int = setInterval(() => {
+          // time interval
+          const next = it.next(); // next value
+          if (!next.done) {
+            // done = true when the end of array reache
+            this.message2 += " " + next.value; // concatenate word to the string
+          } else {
+            clearInterval(int); // when done - clear interval
+          }
+        }, 100); // interval duration, 1s
+      }, 3000);
+    },
+  },
+  mounted() {
+    this.appendFirstName(); // invoke function when ready
+    this.appendLastName(); // invoke function when ready
+    this.appendMsg();
+    this.appendMsg2();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.introMessage2 {
+  display: flex;
+  z-index: 1;
+}
+
+.introMessage2 p {
+  margin: 5rem 2rem;
+}
+h2 {
+  margin-bottom: 1.6rem;
+}
+.intro-container {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
